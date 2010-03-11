0d178c800859b1584ab205d22f586cbd7e0c67d4
pages.bertrand <pages.bertrand@6307593a-2cef-11df-aeb3-278f4910c8cc>
2010-03-11 09:28:57 +0000

    Ajout des fichiers initiaux

---
+++ trunk/firesimulation/firesimulation/firesimulation.cpp
@@ -0,0 +1,18 @@
+// firesimulation.cpp : fichier projet principal.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace firesimulation;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Activation des effets visuels de Windows XP avant la création de tout contrôle
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Créer la fenêtre principale et l'exécuter
+	Application::Run(gcnew Form1());
+	return 0;
+}
