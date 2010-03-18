ca27d940dc90f689956fe0e8e3250655d18831d6
nicolas.noullet <nicolas.noullet@6307593a-2cef-11df-aeb3-278f4910c8cc>
2010-03-18 13:54:56 +0000

    correction forêt

--- Foret.cpp
+++ Foret.cpp
@@ -6,15 +6,14 @@
 
 #include "Foret.h"
 #include "Case.h"
-#include "Matrix.h"
 
 Foret::Foret()
 {
-    this.matrice = Matrix<Case*>(this->NB_LIGNES,this->NB_COLONNES)
+    this->matrice = Matrix<Case*>(this->NB_LIGNES,this->NB_COLONNES);
 }
 Case Foret::getCase(int x, int y)
 {
-    return this->matrice[x][y];
+    return *this->matrice[x][y];
 }
 void Foret::afficher()
 {
@@ -22,7 +21,7 @@ void Foret::afficher()
     {
         for(int j=0 ; j<this->NB_COLONNES ; j++)
         {
-                cout << this.matrice[i][j].getEtat();
+                cout << this->matrice[i][j]->getEtat();
         }
         cout << endl;
     }
