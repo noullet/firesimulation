f37835d355fed1efb99199cf84a7f8fe4e5ba232
nicolas.noullet <nicolas.noullet@6307593a-2cef-11df-aeb3-278f4910c8cc>
2010-03-18 12:50:38 +0000

    Remplissage matrix

--- trunk/Matrix.h
+++ trunk/Matrix.h
@@ -1,4 +1,41 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#include <iostream>
+#include <vector>
+using namespace std;
 
-#endif // MATRIX_H
+template<class T> class Matrix
+{
+	protected:
+		vector <vector<T>*> row;
+
+	public:
+		Matrix(int r = 1, int c = 1) : row(r)
+		{
+			for(int i = 0 ; i < r ; i++)
+			{
+				row[i] = new vector<T>(c);
+			}
+		}
+
+		~Matrix()
+		{
+			for(int i = 0 ; i < row.size() ; i++)
+			{
+				delete row[i];
+			}
+		}
+
+		vector<T>&operator[](int i) const
+		{
+			return *row[i];
+		}
+
+		int rows()
+		{
+			return row.size();
+		}
+
+		int columns()
+		{
+			return row[0]->size();
+		}
+};
