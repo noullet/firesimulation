8b99f7dec166330506f754b1da91fa940af263a1
nicolas.noullet <nicolas.noullet@6307593a-2cef-11df-aeb3-278f4910c8cc>
2010-03-20 15:26:34 +0000

    Ajout methode continuer au main qui appelle - diffuserFeu si la case est feu - decouvrirEnvironnement sur l'agent

--- Foret.h
+++ Foret.h
@@ -12,10 +12,10 @@ class Case;
 class Foret
 {
 private:
-    static const int NB_LIGNES = 10;
-    static const int NB_COLONNES = 10;
     Case*** matrice;
 public:
+    static const int NB_LIGNES = 10;
+    static const int NB_COLONNES = 10;
     Foret();
     ~Foret();
     void afficher();
--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include "Foret.h"
+#include "Case.h"
+#include "Agent.h"
 
 using namespace std;
+
+bool estTermine();
+void continuer(Foret& f);
 
 int main()
 {
     Foret f;
-    f.afficher();
+    char c;
+    while(!estTermine())
+    {
+        f.afficher();
+        cin >> c;
+        continuer(f);
+    }
     return 0;
+}
+
+bool estTermine()
+{
+    return false;
+}
+
+void continuer(Foret &f)
+{
+    for(int i=0 ; i<f.NB_LIGNES ; i++)
+    {
+        for(int j=0 ; j<f.NB_COLONNES ; j++)
+        {
+            if(f.getCase(i,j).estFeu())
+            {
+               f.getCase(i,j).diffuserFeu();
+            }
+            Agent* agent = f.getCase(i,j).getAgent();
+            if(agent != NULL)
+            {
+                (*agent).decouvrirEnvironnement();
+            }
+        }
+    }
 }
